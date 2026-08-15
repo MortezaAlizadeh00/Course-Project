bd4c4b34be19187feee32f0146ed75a7709576a5
MortezaAlizadeh00 <morisoloheroafg@gmail.com>
2026-08-15 23:01:01 +0330

    fix

--- routers/auth.js
+++ routers/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const authController = require("../controllers/auth");
+const coursesController = require("../controllers/course");
 
 const router = express.Router();
+
+router.get("/", coursesController.getAll);
 
 router.get("/signup", authController.showSignup);
 router.post("/signup", authController.signup);
